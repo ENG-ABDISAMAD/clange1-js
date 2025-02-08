e3e926ed4bda351060463a0140c9adcf90fbf014
ABDUSAMAD <abdisamadomarmohamed890@gmail.com>
2025-02-08 08:50:42 -0800

    Add files via upload

---
+++ chlng js 1/jj.js
@@ -0,0 +1,5 @@
+var randomnumber =Math.floor(Math.random()* 4)+1;
+var arkimagesourse = "CHLNG/"+ "ark"+randomnumber +".jpeg"
+var img=document.querySelectorAll("img")[0];
+img.setAttribute("arc",arkimagesourse);
+
